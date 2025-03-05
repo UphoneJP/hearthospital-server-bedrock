d6f1a6997bf2a7b4fe17aaab95e79235c2478f9d
hearthospital <support@hearthospital.js>
2025-03-05 10:25:50 +0900

    sessionの設定を変更

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.use(session({
   cookie : {
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 7,
-    secure: process.env.NODE_ENV === 'production',
+    // secure: process.env.NODE_ENV === 'production',
     // sameSite: 'Lax'
   }
 }))
